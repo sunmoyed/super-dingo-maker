2bf300a1099ee6abc7ede2f702d493a261aa92c6
Marco Munizaga <git@marcopolo.io>
2020-12-30 21:06:21 -0800

    First commit, boilerplate

---
+++ server/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Server backend for dingo maker";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-20.09";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.nixpkgs.follows = "/nixpkgs";
+  };
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+    inputs.nixpkgs.follows = "/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = pkgs.mkShell
+            {
+              buildInputs = [ pkgs.pulumi-bin ];
+            };
+        }
+      );
+}
